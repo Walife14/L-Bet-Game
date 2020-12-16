c6e3b0c5b6d97299653ef73d0323477f816c1678
Lucas <walifedev@gmail.com>
2020-12-16 04:23:05 +0000

    random number and dom
    Created a random number generator between 1-100 for the result.
    I have also selected the guess options with the DOM.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 /* DOM Selectors */
 const currentMoney = document.getElementById("currentMoney");
+const guessLow = document.getElementById("guessLow");
+const guessHigh = document.getElementById("guessHigh");
 
 /* Variable declarations */
 let money = 200;
 
 /* Main */
 currentMoney.innerHTML = money;
+
+/* Creating a random number between 1-100 */
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+
+/* I will need to create a function that runs 
+when the player guesses something, this function will create the random
+number and that random number will be the result
+if the guess is right they win if not they lose */
